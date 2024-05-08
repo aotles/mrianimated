dc51b7b2df933a8a5ce795b0f34047eacddf1a9a
Arel Otles <aotles@gmail.com>
2024-05-07 23:39:46 -0500

    removed evals since that was breaking deployment

--- graph.js
+++ graph.js
@@ -7,8 +7,8 @@ var T2 = document.getElementById("T2_slider").value;
 var Tmax = T1*2;
 var deltaT = Tmax/50;
 const w = 1.0;
-generateData("Math.exp(-x/T2)*Math.cos(-w*x)", 0, Tmax, MxT_Values, MxValues, deltaT);
-generateData("1 - Math.exp(-x/T1)", 0, Tmax, MzT_Values, MzValues, deltaT);
+generateMxData(0, Tmax, MxT_Values, MxValues, deltaT);
+generateMzData(0, Tmax, MzT_Values, MzValues, deltaT);
     
 let xChart = new Chart("XMagnitude", {
   type: "line",
@@ -83,31 +83,39 @@ let zChart = new Chart("ZMagnitude", {
     }
   }
 });
-   
-function generateData(value, i1, i2, xValues, yValues, step = 1) {
+      
+function generateMxData(i1, i2, xValues, yValues, step = 1) {
   xValues.length  = 0;
   yValues.length  = 0;
   for (let x = i1; x <= i2; x += step) {
-    yValues.push(eval(value));
+    yValues.push(Math.exp(-x/T2)*Math.cos(-w*x));
     xValues.push(x);
   }
 }
+  
+function generateMzData(i1, i2, xValues, yValues, step = 1) {
+  xValues.length  = 0;
+  yValues.length  = 0;
+  for (let x = i1; x <= i2; x += step) {
+    yValues.push(1 - Math.exp(-x/T1));
+    xValues.push(x);
+  }
+}
+
 
 //update chart in real time as user changes T1 and T2 values
 document.getElementById("T1_slider").addEventListener("input", function() {
   T1 = document.getElementById("T1_slider").value;
   Tmax = T1*2;
   deltaT = Tmax/50;
-  generateData("Math.exp(-x/T2)*Math.cos(-w*x)", 0, Tmax, MxT_Values, MxValues, deltaT);
+  generateMxData(0, Tmax, MxT_Values, MxValues, deltaT);
   xChart.update();
-  generateData("1 - Math.exp(-x/T1)", 0, Tmax, MzT_Values, MzValues, deltaT);
+  generateMzData(0, Tmax, MzT_Values, MzValues, deltaT);
   zChart.update();
 });
 
 document.getElementById("T2_slider").addEventListener("input", function() {
   T2 = document.getElementById("T2_slider").value;
-  generateData("Math.exp(-x/T2)*Math.cos(-w*x)", 0, Tmax, MxT_Values, MxValues, deltaT);
+  generateMxData(0, Tmax, MxT_Values, MxValues, deltaT);
   xChart.update();
-  generateData("1 - Math.exp(-x/T1)", 0, Tmax, MzT_Values, MzValues, deltaT);
-  zChart.update();
 });
