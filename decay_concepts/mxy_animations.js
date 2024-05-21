7dceecf365cbbe01827240b0d80db9551134aaaf
Arel Otles <aotles@gmail.com>
2024-05-21 14:21:08 -0500

    added dephasing vectors to animation

--- decay_concepts/mxy_animations.js
+++ decay_concepts/mxy_animations.js
@@ -18,6 +18,14 @@ const xdir = new THREE.Vector3( 1, 0, 0 );
 const zdir = new THREE.Vector3( 0, 1, 0 );
 const ydir = new THREE.Vector3( 0, 0, 1 );
 const rfField = new THREE.Vector3( 0, 0, 1 );
+
+//create the dephasing vectors
+var dephaseVecs = [];
+var numDephaseVecs = 10;
+for (let x = 0; x < numDephaseVecs; x++) {
+  dephaseVecs.push(new THREE.Vector3(0, 0, 0));
+}
+
 //normalize the direction vector (convert to vector of length 1)
 dir.normalize();
 const origin = new THREE.Vector3( 0, -1, 0 );
@@ -30,10 +38,19 @@ const protonArrow = new THREE.ArrowHelper( dir, origin, length, hex );
 const xArrow = new THREE.ArrowHelper( xdir, origin, length, red );
 const yArrow = new THREE.ArrowHelper( ydir, origin, length, green );
 const zArrow = new THREE.ArrowHelper( zdir, origin, length, blue );
+
+//set up arrows for dephasing vectors
+const dePhaseArrows = [];
+for (let x = 0; x < dephaseVecs.length; x++) {
+  dePhaseArrows.push(new THREE.ArrowHelper(dephaseVecs[x], origin, length, 0xff8800));
+}
 scene.add( protonArrow );
 scene.add( xArrow ); 
 scene.add( yArrow ); 
 scene.add( zArrow ); 
+for (let x = 0; x < dePhaseArrows.length; x++) {
+  scene.add( dePhaseArrows[x] ); 
+}
 
 camera.position.y = 1;
 camera.position.z = 5;
@@ -52,14 +69,21 @@ var T2 = document.getElementById("T2_slider").value;
 var protonVec = new THREE.Vector3(0, 0, 0);
 function animate() {
 	requestAnimationFrame( animate );
-  //lamor procession - > is the 1 in the z direction
-  //protonArrow.setDirection(dir.applyAxisAngle(new THREE.Vector3(0, 1, 0), 0.01));
-  //protonArrow.setDirection(new THREE.Vector3(delta*10.0, 1, 0).normalize());
   protonVec.setFromCylindricalCoords(1, w*t, 0);
   t = clock.getElapsedTime();
   T2 = document.getElementById("T2_slider").value;
   protonArrow.setDirection(protonVec);      //My
   protonArrow.setLength(length* Math.exp(-t/T2));      //My
+
+  for (let x = 0; x < dephaseVecs.length; x++) {
+    var offset = (x*(2*Math.PI/(numDephaseVecs-1)) + -Math.PI)*(1 -Math.exp(-t/T2));
+    dephaseVecs[x].setFromCylindricalCoords(1, w*t + offset, 0);
+    dePhaseArrows[x].setDirection(dephaseVecs[x]);      //My
+    dePhaseArrows[x].setLength(length);      //My
+  }
+
+
+
 	renderer.render( scene, camera );
 }
 
